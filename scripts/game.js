7bbcdeca16a699aa162e35d20882002a03d69950
Prodegyi <90901987+Prodegyi@users.noreply.github.com>
2025-09-12 20:26:27 +0200

    renamed files : models/movableObjects.js => models/movableObjects.class.js (100%)

--- scripts/game.js
+++ scripts/game.js
@@ -1,12 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new moveableObject();
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
 
-     character.src = "./img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-1.png"
+    console.log(character);
 
-     ctx.drawImage(character, 50, 50, 100, 100);
 }
