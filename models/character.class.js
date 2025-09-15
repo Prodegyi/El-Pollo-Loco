f11eaddf00d2533f935b982ce7d4f090bc9a1561
Prodegyi <90901987+Prodegyi@users.noreply.github.com>
2025-09-15 19:20:34 +0200

    added some funciionality to display the characte  on the canvas doesnt show up yet

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,9 @@
 class Character extends moveableObject{
 
 
+    constructor(){
+        super().loadImage('img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-1.png');
+    }
     jump() {
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -2,9 +2,15 @@ class World{
 
      character = new Character();
      enemies = [new chicken(), new chicken(), new chicken()];
+     ctx;
 
+     constructor(canvas){
+        this.ctx = canvas.getContext('2d')
+        this.Draw();
+
+     }
 
     Draw(){
-        console.log("draw world");
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
     }
 }
